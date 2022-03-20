8c5d173ce4d48b4ca6dd97418c8ac89865c7b258
codeLuosiyu <2422344142@qq.com>
2022-03-20 22:56:37 +0800

    feat(toa-react):update react ui project

---
+++ react/toa-react/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const a = "123"
+  if (a === "123") {
+
+  }
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
